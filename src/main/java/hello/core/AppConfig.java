87013b01841600ed3c82bc302f43a72b5c43ec89
DevBigOne <a62387272@gmail.com>
2024-03-26 15:40:22 +0900

    [FIX] SingletonTEST 오류 해결

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,21 +15,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration           // 설정 정보
 public class AppConfig { // 나의 애플케이션을 설정 및 구성 (실제로 동작에 필요한 구현 객체를 만듬)
                          // 생성한 객체 인스턴스의 참조를 생성자를 통해서 주입해줌.
+    // @Bean memberService -> new MemoryMemberServiceRepository
+    // @Bean orderService -> new MemoryMemberServiceRepository
 
     @Bean // Bean 사용하면 스프링 컨테이너에 등록 됨
     public MemberService memberService() {
-        return new MemberServiceImpl(MemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public static MemberRepository MemberRepository() { //나의 애플케이션에서 사용할 메소드 , 나중에 다른 구현체로 변경한다면 여기만 변경하면 됨.
+    public static MemberRepository memberRepository() { //나의 애플케이션에서 사용할 메소드 , 나중에 다른 구현체로 변경한다면 여기만 변경하면 됨.
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() { //나의 애플케이션에션에서 사용할 오더 서비스
         return new OrderServiceImpl(
-                MemberRepository(),
+                memberRepository(),
                 new RateDiscountPolicy());
     }
 
