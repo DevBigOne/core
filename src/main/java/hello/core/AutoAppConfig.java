640db41928b1697cbf855e0fc7eff1730e39ccfd
DevBigOne <a62387272@gmail.com>
2024-03-28 01:17:35 +0900

    [ADD] 필터 학습

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,9 +6,11 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        // basePackages = "hello.core.member", //해당 패키지부터 찾음 member만 찾는다는 것, (어디서 찾을 수 있도록 지정하는 것)
+        // basePackageClasses = AutoAppConfig, //core 패키지 부터 찾는 것
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //@Configuration을 빼는 이유는 수동으로 등록한 @Configuration와 충돌이 날 수 있기 때문(실무에서는 제외를 하지 않음)
 ) //스프링 빈을 스캔에서 쫙 끌어올리는 역할
 public class AutoAppConfig {
-
 }
+// TIP : 개인적으로 즐겨 사용하는 방법은 패키지를 지정하지 않고, 설정 정보 클래스이 위치를 프로젝트 최상단에 두는 것
---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,36 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+    @Test
+    void filterScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class); // 값 조회, 스프링 빈 등록
+        assertThat(beanA).isNotNull();
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class) // 스프링 빈에 등록 X
+        );
+    }
+
+    @Configuration
+    @ComponentScan( //어노테이션 생략가능 기본이라,
+            includeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+    }
+}
