938870f9f4c5ab8a3509c8231b85fe9694bbc0e2
DevBigOne <a62387272@gmail.com>
2024-03-28 01:26:50 +0900

    [ADD] 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,5 +15,11 @@ import org.springframework.context.annotation.FilterType;
         //@Configuration을 빼는 이유는 수동으로 등록한 @Configuration와 충돌이 날 수 있기 때문(실무에서는 제외를 하지 않음)
 ) //스프링 빈을 스캔에서 쫙 끌어올리는 역할
 public class AutoAppConfig {
+
+    @Bean( name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 }
 // TIP : 개인적으로 즐겨 사용하는 방법은 패키지를 지정하지 않고, 설정 정보 클래스이 위치를 프로젝트 최상단에 두는 것
