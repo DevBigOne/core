3b9d270606800a8a62313d4e24c01e8fc7b59b51
DevBigOne <a62387272@gmail.com>
2024-03-28 20:42:25 +0900

    [ADD] 생성자 주입

---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000); // 수정자 메소드를 두면 더미라도 데이터를 넣어줘야함.
+        assertThat(order.getDiscountPrice()).isEqualTo(1000); // 스프링 말고 자바 코드로 직접 테스트 코드 제작
+    }
+
+}
