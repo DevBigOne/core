9423bfde133f63ba51901c8287d0a05f264b82bf
DevBigOne <a62387272@gmail.com>
2024-03-28 19:55:09 +0900

    [ADD] 다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository; // 생성자는 한번 만들어지면 바꿀 수 없다 - 불변. 필수 의존 관계를 사용함. 무조건 값이 있어야함.
     private final DiscountPolicy discountPolicy; //DIP만족 및 OCP 확장 열려있고 변경에는 닫아져있다.
 
+    // 생성자가 딱 한개 있으면 Autowired를 생략해도 됨.
+    // setter를 쓸 때 set- , 필드 값을 메서드를 통해서 하는데 Autowired를 사용 가능함. 빈을 등록하면 의존관계가 같이 일어남.
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { //외부에서 객체를 선언해서 받아옴 (DI)
