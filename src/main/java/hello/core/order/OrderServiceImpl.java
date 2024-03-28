c18f023d0d7e05a2893d987e319c773f8f757ad5
DevBigOne <a62387272@gmail.com>
2024-03-28 21:09:02 +0900

    [ADD] 룸복과 최신 트랜드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,24 +6,21 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //final이 붙은 객체를 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; // 생성자는 한번 만들어지면 바꿀 수 없다 - 불변. 필수 의존 관계를 사용함. 무조건 값이 있어야함.
-    //final은 변하지 않는다는 것 장점 
+    //final은 변하지 않는다는 것 장점
     private final  DiscountPolicy discountPolicy; //DIP만족 및 OCP 확장 열려있고 변경에는 닫아져있다.
 
     // 생성자가 딱 한개 있으면 Autowired를 생략해도 됨.
     // setter를 쓸 때 set- , 필드 값을 메서드를 통해서 하는데 Autowired를 사용 가능함. 빈을 등록하면 의존관계가 같이 일어남.
 
-   @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { //외부에서 객체를 선언해서 받아옴 (DI)
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
